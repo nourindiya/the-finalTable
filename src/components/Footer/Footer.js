91bd60c790058bf3da8963f5e641977b5969e9a6
Nowrin Nazim <diya.nowrin08@gmail.com>
2021-09-26 00:22:08 +0600

    site commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Final Table © 2021 </p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
+import Footer from '../Footer/Footer';
 import Person from '../Person/Person';
 import "./Shop.css"
 
@@ -43,7 +44,9 @@ const Shop = () => {
                     </Cart>
                 </div>
             </div>
+            <Footer></Footer>
         </div >
+
     );
 };
 
